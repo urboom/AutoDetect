47d65ea141a03d5fa0f4e418453957816267c57b
urboom <yurec116@gmail.com>
2016-03-22 21:27:21 +0200

    change layout and icon

--- app/src/main/java/xyz/yunikitin/autodetect/MainActivity.java
+++ app/src/main/java/xyz/yunikitin/autodetect/MainActivity.java
@@ -1,13 +1,38 @@
 package xyz.yunikitin.autodetect;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button registrationButton;
+    private Button takePhotoButton;
+    private Button openDatabaseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registrationButton = (Button) findViewById(R.id.registration_button);
+        takePhotoButton = (Button) findViewById(R.id.take_photo_button);
+        openDatabaseButton = (Button) findViewById(R.id.open_DB_button);
+
+        registrationButton.setOnClickListener(this);
+        takePhotoButton.setOnClickListener(this);
+        openDatabaseButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.registration_button:
+                Intent intent1 = new Intent(this, RegistrationActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
